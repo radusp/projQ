78721d1e8ecaa63087e6b92021a08dd8fc4705f7
RaduS <radu.socaciu.p@gmail.com>
2017-12-10 09:16:39 +0200

    test proj

---
+++ TEST_QR_Project_BL/ImageAnalyzerTest.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using QR_Project_BL;
+
+namespace TEST_QR_Project_BL
+{
+    /// <summary>
+    /// Summary description for ImageAnalyzerTest
+    /// </summary>
+    [TestClass]
+    public class ImageAnalyzerTest
+    {
+        public ImageAnalyzerTest()
+        {
+            //
+            // TODO: Add constructor logic here
+            //
+        }
+
+        private TestContext testContextInstance;
+
+        /// <summary>
+        ///Gets or sets the test context which provides
+        ///information about and functionality for the current test run.
+        ///</summary>
+        public TestContext TestContext
+        {
+            get
+            {
+                return testContextInstance;
+            }
+            set
+            {
+                testContextInstance = value;
+            }
+        }
+
+        #region Additional test attributes
+        //
+        // You can use the following additional attributes as you write your tests:
+        //
+        // Use ClassInitialize to run code before running the first test in the class
+        // [ClassInitialize()]
+        // public static void MyClassInitialize(TestContext testContext) { }
+        //
+        // Use ClassCleanup to run code after all tests in a class have run
+        // [ClassCleanup()]
+        // public static void MyClassCleanup() { }
+        //
+        // Use TestInitialize to run code before running each test 
+        // [TestInitialize()]
+        // public void MyTestInitialize() { }
+        //
+        // Use TestCleanup to run code after each test has run
+        // [TestCleanup()]
+        // public void MyTestCleanup() { }
+        //
+        #endregion
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+            VideoManager newVideo = new VideoManager();
+            newVideo.loadVideo(@"D:\scan1.mp4");
+            newVideo.extractFramesFromVideo();
+
+            ImageAnalyzer analyzeImages = new ImageAnalyzer(newVideo);
+            analyzeImages.analyzeAllImages();
+         //   aa.getOneImage();
+        }
+    }
+}
---
+++ TEST_QR_Project_BL/VideoManagerTest.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using QR_Project_BL;
+
+namespace TEST_QR_Project_BL
+{
+    [TestClass]
+    public class VideoManagerTest
+    {
+        [TestMethod]
+        [ExpectedException(typeof(EmptyPathSpecifiedWhenLoadingVideo))]
+        public void emptyPathToVideoReceivedThrowError()
+        {
+            //Arrange
+            VideoManager myManager = new VideoManager();
+            //Act
+            myManager.loadVideo("");
+            //Assert
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(PathToVideoFileDoesNotExistException))]
+        public void pathThatDoesNotExistWillThrowCustomException()
+        {
+            //Arrange
+            VideoManager myManager = new VideoManager();
+            //Act
+            myManager.loadVideo(@"O:\zzz.avi");
+            //Assert
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(NullPathToVideoException))]
+        public void nullPathToVideoFIleWillThrowCustomException()
+        {
+            //Arrange
+            VideoManager myManager = new VideoManager();
+            //Act
+            myManager.loadVideo(null);
+            //Assert
+        }
+        [TestMethod]
+        public void IfAbleToLoadVideoSetSuccessFlagToTrue()
+        {
+            //Arrange
+            VideoManager myManager = new VideoManager();
+            //Act
+            myManager.loadVideo(@"C:\test.mp4");
+            //Assert
+            Assert.AreEqual(myManager.isVideoLoaded(), true);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ExtractFramesWhenNoVideoWasLoaded))]
+        public void TryingToExtractWhenNoVideoIsLoadedWillThrowError()
+        {
+            //Arrange
+            VideoManager myManager = new VideoManager();
+            //Act
+            myManager.loadVideo(@"C:\test.mp4");
+            myManager.setForTestIsVideoLOaded(false);
+            myManager.extractFramesFromVideo();
+            //Assert
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(FrameCountOFLoadedVideoIsZero))]
+        public void IfFrameCountOfLoadedVideoIsZeroThrowCustomException()
+        {
+            //Arrange
+            VideoManager myManager = new VideoManager();
+            //Act
+            myManager.loadVideo(@"C:\sample.wmv");
+            myManager.extractFramesFromVideo();
+            //Assert
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(NoFramesExtractedInListFromVideo))]
+        public void IfNoFramesAreINListThrowCustomException()
+        {
+            //Arrange
+            VideoManager myManager = new VideoManager();
+            //Act
+            myManager.loadVideo(@"C:\test.mp4");
+            myManager.extractFramesFromVideo();
+            myManager.setForTestisListOfFramesPopulated(false);
+            //Assert
+        }
+    }
+}
