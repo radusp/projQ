38c0675c4a75b15224fff2b998b46118474a45eb
RaduS <radu.socaciu.p@gmail.com>
2017-12-11 18:18:44 +0200

    TEst

---
+++ TEST_QR_Project_BL/Fakes/FakeImageQueue.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using QR_Project_BL.Interfaces;
+using System.Drawing;
+
+
+namespace TEST_QR_Project_BL.Fakes
+{
+    public class FakeImageQueue : IimageQueue
+    {
+        
+        public void AddFrame(Bitmap transaction)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void CompleteAdding()
+        {
+            throw new NotImplementedException();
+        }
+
+        public int getQueueCount()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void processImagesFromQueue()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+}
